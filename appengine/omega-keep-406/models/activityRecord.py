272f7f7f5add03137e3b9aa1415acbe4eb15cd2a
patt0 <patrick@cloudmatters.co>
2013-12-10 14:52:19 +0530

    ActivityRecord Model & RPC to create and update from GAS

---
+++ appengine/omega-keep-406/models/activityRecord.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import ndb
+
+class ActivityRecord(ndb.Model):
+    # tempted to use the G+ unique activity id ( stack overflow ?)
+    id = ndb.StringProperty()
+    gplusId = ndb.StringProperty()          # we identify GDE's uniquely using this
+    name = ndb.StringProperty()
+    date = ndb.StringProperty()				# date at which the activity (post) was made
+    plusOners = ndb.IntegerProperty()
+    resharers = ndb.IntegerProperty()
+    title = ndb.StringProperty()
+    # url of the post (question for stack overflow)
+    url = ndb.StringProperty()
+    productGroup = ndb.StringProperty()
+    activityType = ndb.StringProperty()
+    links = ndb.StringProperty()
+    # each activity type has a different Metadata
+    activityMetadata = ndb.JsonProperty()
