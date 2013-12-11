e6dd89e7d70e8771503c01d4989b1daf2f8867d2
Shay Erlichmen <erlichmen@gmail.com>
2013-12-11 23:21:49 +0200

    don’t use the root path

--- appengine/services/activity_record.py
+++ appengine/services/activity_record.py
@@ -6,13 +6,13 @@ api_root = endpoints.api(name='gdetracking', version='v1.0b1')
 @api_root.api_class(resource_name='activity_record', path='activityRecord')
 class ActivityRecordService(remote.Service):
 
-    @ActivityRecord.method(path='/', http_method='POST', name='insert')
+    @ActivityRecord.method(path='/activityRecord', http_method='POST', name='insert')
     def insert(self, activity_record):
         activity_record.put()
         return activity_record
 
 
-    @ActivityRecord.method(request_fields=('id',),  path='/{id}', http_method='GET', name='get')
+    @ActivityRecord.method(request_fields=('id',),  path='/activityRecord/{id}', http_method='GET', name='get')
     def get(self, activity_record):
         if not activity_record.from_datastore:
             raise endpoints.NotFoundException('ActivityRecord not found.')
