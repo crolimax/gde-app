84498c666eaed863178f186f91a16305be256c99
patt0 <patrick@cloudmatters.co>
2014-07-08 20:23:34 +0530

    GetNewActivites without saving

--- appengine/services/gplus.py
+++ appengine/services/gplus.py
@@ -2,23 +2,38 @@
 
 The functions called by two CRON jobs defined in cron.yaml
 
-UpdateActivityPosts iterates through all the activites and
-finds out wether the number of +1 and reshares has changed
-and updates the ActivityRecord (it should exit, log error)
-so that the total impact is updated.
+UpdateActivityPosts iterates through all the activites and finds out wether 
+the number of +1 and reshares has changed and updates the ActivityRecord (it 
+should exit, log error) so that the total impact is updated.
+
+NewActivityPosts iterates through all the experts accounts and obtains all 
+the public posts from gplus.  If the post is #gde then create an entity for
+the new post.
     
 """
 import webapp2
 import logging
+import re
 from datetime import datetime
 from google.appengine.api import mail
 from apiclient.discovery import build
 
 from models import ActivityPost
 from models import ActivityRecord
+from models import Account
 from models import activity_record as ar
 
 API_KEY = 'AIzaSyBVCJKggp_1VBIC2xisWCWrX-VR-Dih694'
+
+ACTIVITY_TYPES = ["#bugreport", "#article", "#blogpost", "#book", "#techdocs",
+                  "#translation", "#techtalk", "#opensourcecode",
+                  "#forumpost", "#community", "#video", "#tutorial", "#interview"];
+
+PRODUCT_GROUPS = ["#android", "#admob", "#adwords", "#angulars", "#chrome",
+                  "#dart", "#dartlang", "#cloudplatform", "#googleanalytics",
+                  "#googleappsapi","#googleappsscript", "#googledrive",
+                  "#glass", "#googlemapsapi", "#googleplus", "#youtube",
+                  "#uxdesign"];
 
 class UpdateActivityPosts(webapp2.RequestHandler):
 
@@ -28,6 +43,7 @@ class UpdateActivityPosts(webapp2.RequestHandler):
         """Process the CRON job."""
         logging.info('Start UpdateActivityPosts')
         metrics = {"start" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+        records = []
         #build the service object for the gplus api
         service = build('plus', 'v1', developerKey=API_KEY)
         #get all the activities (last 6 months enough?)
@@ -48,9 +64,12 @@ class UpdateActivityPosts(webapp2.RequestHandler):
                 updated_activity.put()
                 activity_record = ar.find_or_create(updated_activity)
                 activity_record.add_post(updated_activity)
+                #create a list of updated posts
+                records.append(updated_activity.url)
 
         metrics["count"] = count
         metrics["updated_count"] = updated_count
+        metrics["records"] = records
         metrics["end"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.send_transcript_mail(metrics)
         logging.info('End UpdateActivityPosts')
@@ -76,11 +95,154 @@ class UpdateActivityPosts(webapp2.RequestHandler):
         body_string = "Started at %s \n" % metrics["start"]
         body_string += "ActivityPosts processed : %s \n" % metrics["count"]
         body_string += "ActivityPosts updated : %s \n" % metrics["updated_count"]
+
+        records = metrics["records"]
+        for record in records:
+            body_string += "%s \n" % record 
+
         body_string += "Ended at %s \n" % metrics["end"]
 
-        mail.send_mail(sender="GDE Tracking App Support <omega-keep-406@appspot.gserviceaccount.com>",
+        print body_string
+
+        mail.send_mail(sender="GDE Tracking App Support <no-reply@omega-keep-406.appspotmail.com>",
               to="Patrick Martinent <patrick.martinent@gmail.com>",
-              subject="Update ActivityPost for %s " % datetime.now().strftime("%Y-%m-%d"),
+              subject="GAE CRON JOB : Update ActivityPost for %s " % datetime.now().strftime("%Y-%m-%d"),
               body="""%s""" % body_string)
+
+
+class NewActivityPosts(webapp2.RequestHandler):
+
+    """Gets New Activity Posts."""
+
+    def get(self):
+        """Process the CRON job."""
+        logging.info('Start NewActivityPosts')
+        metrics = {"start" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+        records = []
+        #build the service object for the gplus api
+        service = build('plus', 'v1', developerKey=API_KEY)
+        accounts = Account.query()
+        user_count = 0
+        new_activities = 0
+        for account in accounts:
+            #don't process admin users
+            if account.type == "administrator":
+                continue
+            user_count += 1
+            # find out what the last activity date recorded is for the expert
+            activities = ActivityPost.query().order(-ActivityPost.date).fetch()
+            last_activity_date = activities[0].date
+            # get the last 100 activities from gplus
+            # eventually we can also query using the date so we don't
+            # have to compare each post with the date we just got
+            result = service.activities().list(userId=account.gplus_id,
+                                               collection='public',
+                                               maxResults=100).execute()
+            gplus_activities = result.get('items', [])
+            for gplus_activity in gplus_activities:
+                if gplus_activity["updated"] > last_activity_date:
+                    # ensure this is a gde post
+                    if self.is_gde_post(gplus_activity):
+                        new_activities += 1
+                        #create a list of updated posts
+                        records.append(gplus_activity["url"])
+                        activity_post = self.create_activity(gplus_activity, account)
+                        print activity_post
+                        # activity = ActivityPost(activity_data)
+                        # activity.put()
+        metrics["user_count"] = user_count
+        metrics["new_activities"] = new_activities
+        metrics["records"] = records
+        metrics["end"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+               
+        self.send_transcript_mail(metrics)
+        print user_count
+
+    def create_activity(self, activity, account):
+        plus_oners = activity['object']['plusoners']['totalItems']
+        resharers = activity['object']['resharers']['totalItems']
+        activity_post = ActivityPost(id=activity["id"],
+                                post_id=activity["id"],
+                                name=account.real_name,
+                                gplus_id=account.gplus_id,
+                                date=activity["updated"],
+                                url=activity["url"],
+                                title=activity["title"],
+                                plus_oners=plus_oners,
+                                resharers=resharers)
+        at = self.get_activity_types(activity["object"]["content"])
+        activity_post.populate(activity_type=at)
+        pg = self.get_product_groups(activity["object"]["content"])
+        activity_post.populate(product_group=pg)
+        try:
+            attachments = activity["object"]["attachments"]
+        except:
+            print
+        else:
+            links = self.get_links(attachments)
+            activity_post.populate(links=links)
+        
+        return activity_post
+
+    def is_gde_post(self, activity):
+        # first find out wether the post contains #gde
+        result = re.search('(#(gde</a>))', activity["object"]["content"], flags=re.IGNORECASE)
+        if result is None:
+            return False
+        # find out wether the verb of the post is 'post'
+        # alternatively is the verb is 'share' then verify that
+        # actor.id of the share and the original post are the same
+        valid_post = False
+        if activity["verb"] == 'post':
+            return True
+        elif activity["object"]["actor"]["id"] == activity["id"]:
+            return True
+        else:
+            return False
+
+    def get_activity_types(self, content):
+        at = []
+        for activity_type in ACTIVITY_TYPES:
+            result = re.search(activity_type, content, flags=re.IGNORECASE)
+            if result is not None:
+                at.append(activity_type)
+        return at
+
+    def get_product_groups(self, content):
+        pg = []
+        for product_group in PRODUCT_GROUPS:
+            result = re.search(product_group, content, flags=re.IGNORECASE)
+            if result is not None:
+                pg.append(product_group)
+        return pg
+
+    def get_links(self, attachments):
+        links = ""
+        for attachment in attachments:
+            if attachment["objectType"] == "article" or attachment["objectType"] == "video":
+                if links != "":
+                    links += ", "
+                links += attachment["url"]
+        return links
+
+    def send_transcript_mail(self, metrics):
+        body_string = "Started at %s \n" % metrics["start"]
+        body_string += "Accounts processed : %s \n" % metrics["user_count"]
+        body_string += "New activities found : %s \n" % metrics["new_activities"]
+
+        records = metrics["records"]
+        for record in records:
+            body_string += "%s \n" % record 
+
+        body_string += "Ended at %s \n" % metrics["end"]
+
+        print body_string
+
+        mail.send_mail(sender="GDE Tracking App Support <no-reply@omega-keep-406.appspotmail.com>",
+              to="Patrick Martinent <patrick.martinent@gmail.com>",
+              subject="GAE CRON JOB : Get New Activities %s " % datetime.now().strftime("%Y-%m-%d"),
+              body="""%s""" % body_string)
+
+
 
 
