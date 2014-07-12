1d2ad29e6712b3e91191dc16593de989b3a45c34
Mauro Solcia <mauro.solcia@gmail.com>
2014-07-12 13:14:35 +0200

    Use user account for authentication
    Use user account for authentication and the user.type to display the right menu.
    Due to response 404 when the user is not found, an exception in Angular $http code is thrown and we should implement an `interceptor` [see here] (https://docs.angularjs.org/api/ng/service/$http).
    
    For now we check beforehand if a @google domain user is connecting and enable the right menu, then execute the $http call and execute the right code if we get results

--- js/GdeTrackingApp.js
+++ js/GdeTrackingApp.js
@@ -1428,6 +1428,7 @@ GdeTrackingApp.controller('plusLoginCtrl',						function($scope	,$http	,$rootSco
 				'userId': 'me'
 			});
 			request.execute(function(resp)
+			request.execute(function(resp)
 			{
 //				console.log(resp);
 				$rootScope.$broadcast('gde:logged',resp.displayName);
@@ -1436,39 +1437,70 @@ GdeTrackingApp.controller('plusLoginCtrl',						function($scope	,$http	,$rootSco
 				$scope.userEmails = resp.emails;
 				$scope.id = resp.id;
 				$rootScope.usrId = resp.id;
-				var checkUserEndPointURL = 'https://omega-keep-406.appspot.com/_ah/api/gdetracking/v1.0b1/account/'+resp.id+'?fields=type';
-				for (var i=0;i<$scope.userEmails.length;i++)
+				
+				//Googlers are not stored in
+			    for (var i=0;i<$scope.userEmails.length;i++)
 				{
-//					var emailAddressStartPosition = $scope.userEmails.indexOf("@");
 					var emailDomain = $scope.userEmails[i].value.substring($scope.userEmails[i].value.indexOf('@'));
-					if ($scope.userEmails[i].value == 'maiera@google.com ')
-					{
-						console.log('Welcome Googler '+$scope.userName+'! You are an administrator of this app.');
-						$('#generalStatisticsForGooglers').css('display','block');
-					} else if (emailDomain == '@google.com')
+					if (emailDomain == '@google.com')
 					{
 						console.log('Welcome Googler '+$scope.userName+'!');
 						$('#generalStatisticsForGooglers').css('display','block');
-					} else
-					{
-						$http({method: 'GET', url: checkUserEndPointURL})
-						.success(function(response, status, config)
-						{
-							$scope.type=response.type;
-							console.log('Welcome GDE '+$scope.userName+'!');
-							$('#myStatistics').css('display','block');
-						})
-						.error(function(response, status, config)
-						{
-							console.log('Welcome '+$scope.userName);
-						});
 					}
 				}
+    			
 				$('.userName').text($scope.userName);									// Binds the user name into the DOM using a class via jQuery so it can be repeated throughout the document.
 				var userImage = document.createElement('img');
 				userImage.src = $scope.userImageUrl;
 				userImage.alt = 'g+ image';
 				$('#userImg').html(userImage);
+				
+				var checkUserEndPointURL = 'https://omega-keep-406.appspot.com/_ah/api/gdetracking/v1.0b1/account/'+resp.id+'?fields=type';
+				//Get the user Account Object
+				$http({method: 'GET', url: checkUserEndPointURL})
+				.success(function(response, status, config)
+				{
+					$scope.type=response.type;
+					console.log('Welcome '+$scope.userName+'!');
+					
+					$('#generalStatisticsForGooglers').css('display','none');//Hide the previously shown menu
+					//Show the right menu by user type
+					switch (response.type){
+					    case 'administrator':
+					        console.log('You are an administrator of this app.');
+					        $('#generalStatisticsForGooglers').css('display','block');
+					        break;
+					    case 'manager':
+					        console.log('You are a manager of this app.');
+					        $('#generalStatisticsForGooglers').css('display','block');
+					        break;
+					    case 'active':
+					        console.log('You are a GDE');
+					        $('#myStatistics').css('display','block');
+					        break;
+					    default:
+					        //disabled users
+					        break;
+					}
+				})
+				/*
+				.error(function(response, status, config)//....doesn't work with 404 responses
+				{
+				    console.log('error');
+				    //Googlers are not stored in
+				    for (var i=0;i<$scope.userEmails.length;i++)
+    				{
+    					var emailDomain = $scope.userEmails[i].value.substring($scope.userEmails[i].value.indexOf('@'));
+    					//if (emailDomain == '@google.com')
+    					if (emailDomain == '@mail.com')
+    					{
+    						console.log('Welcome Googler '+$scope.userName+'!');
+    						$('#generalStatisticsForGooglers').css('display','block');
+    					}
+    				}
+				})*/
+				;
+				
 			});
 		});
 	});
